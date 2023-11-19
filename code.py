a9b1a15aa72311779717c72c370981d44b6c7e94
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-19 22:32:24 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import json
 import pandas as pd
 import plotly.graph_objects as go
 
@@ -30,8 +31,20 @@ europe_data_filtered.to_json('json-Europe-Filtered.json', orient='records', line
 
 
 
+# Open and load the JSON file
+with open('json-Europe-Filtered.json', 'r') as file:
+    data = [json.loads(line) for line in file]
+
+# Convert the JSON data to a DataFrame
+europe_filtered_data = pd.DataFrame(data)
+
 # Select specific columns for Europe
-europe_selected_columns = europe_filtered_data[['iso_code', 'continent', 'location', 'date', 'total_cases', 'population']]
+europe_selected_columns = europe_filtered_data[
+    ['iso_code', 'continent', 'location', 'date', 'total_cases', 'population',
+     'total_vaccinations', 'people_vaccinated', 'people_fully_vaccinated',
+     'total_vaccinations_per_hundred', 'people_vaccinated_per_hundred',
+     'people_fully_vaccinated_per_hundred']
+]
 
 # Save the DataFrame to a new JSON file
 europe_selected_columns.to_json('json-Europe-SelectedColumns.json', orient='records', lines=True)
@@ -40,49 +53,50 @@ europe_selected_columns.to_json('json-Europe-SelectedColumns.json', orient='reco
 # Load your Europe data
 europe_df = pd.read_json('json-Europe-SelectedColumns.json', lines=True)
 
-# Replace 'date', 'total_cases', and 'location' with your actual column names
+# Replace 'date', 'total_vaccinations', and 'location' with your actual column names
 # Convert the 'date' column to datetime if it's not already
 europe_df['date'] = pd.to_datetime(europe_df['date'])
 
 # Extract year and month from the date
-europe_df['year_month'] = europe_df['date'].dt.to_period('M')
+europe_df['year_month'] = europe_df['date'].dt.to_period('M').astype(str)  # Convert Period to string
 
-# Group by location and year_month, aggregating total cases
-grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False)['total_cases'].sum()
+# Group by location and year_month, aggregating total vaccinations
+grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False).agg({
+    'iso_code': 'first',
+    'continent': 'first',
+    'date': 'first',
+    'total_cases': 'sum',
+    'population': 'first',
+    'total_vaccinations': 'sum',
+    'people_vaccinated': 'sum',
+    'people_fully_vaccinated': 'sum',
+    'total_vaccinations_per_hundred': 'sum',
+    'people_vaccinated_per_hundred': 'sum',
+    'people_fully_vaccinated_per_hundred': 'sum'
+})
 
-# Create a choropleth using Plotly Express
+# Create a choropleth using Plotly Express based on total vaccinations
 fig = px.choropleth(grouped_df,
                     locations='location',
                     locationmode='country names',
-                    color='total_cases',
+                    color='total_vaccinations',
+                    hover_name='location',
+                    hover_data=grouped_df.columns,
                     animation_frame='year_month',
-                    color_continuous_scale='Viridis',  # You can choose any colorscale
-                    title="Total Cases Choropleth Across Locations in Europe",
-                    width=1200,   # Adjust the width as needed
-                    height=800,   # Adjust the height as needed
-                    projection='natural earth',  # You can choose a different map projection
+                    color_continuous_scale='Viridis',
+                    title="Total Vaccinations Choropleth Across Locations in Europe",
+                    width=1200,
+                    height=800,
+                    projection='natural earth'
                     )
 
 # Use Europe-specific projection and set the initial center and zoom
 fig.update_geos(
     projection_type="natural earth",
-    center=dict(lon=10, lat=50),  # Adjust longitude and latitude to center the map on Europe
-    scope="europe",  # Set the scope to Europe
+    center=dict(lon=10, lat=50),
+    scope="europe",
 )
 
-# Add country names
-fig.add_trace(
-    go.Scattergeo(
-        locationmode='country names',
-        lon=grouped_df['location'],
-        lat=[0] * len(grouped_df),  # Use 0 as latitude to position the text at the bottom of the map
-        mode='text',
-        text=grouped_df['location'],
-        textposition='bottom center',
-        showlegend=False,
-        textfont=dict(size=10, color='black'),  # Adjust font size and color
-    )
-)
 
 # Show the plot
 fig.show()
