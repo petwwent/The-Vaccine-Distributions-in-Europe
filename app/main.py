dbc66ac1b402f0912f44bbb458ca596651306ade
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-20 21:24:14 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
 import json
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Get the absolute path of the 'data' directory
-    data_dir = os.path.abspath('data')
+    # Load the data for visualization
+    with open('data/json-Europe-SelectedColumns.json') as f:
+        data = json.load(f)
 
-    # Join the path to the JSON file
-    json_file_path = os.path.join(data_dir, 'json-Europe-SelectedColumns.json')
-
-    # Check if the file exists
-    if os.path.exists(json_file_path):
-        # Load the data for visualization
-        with open(json_file_path) as f:
-            data = json.load(f)
-
-        return render_template('index.html', data=json.dumps(data))
-    else:
-        return 'File not found'
+    return render_template('index.html', data=json.dumps(data))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)  # Specify the port to run the application
