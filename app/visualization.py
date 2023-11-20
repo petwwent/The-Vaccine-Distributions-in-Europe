adffb517b9e2740ffa24508fced9087d4787b8d3
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-20 21:26:14 +0100

    Update visualization.py

--- app/visualization.py
+++ app/visualization.py
@@ -2,61 +2,56 @@ import plotly.express as px
 import json
 import pandas as pd
 import plotly.graph_objects as go
-import os
+
 
 def construct_choropleth():
-    json_file_path = 'data/json-Europe-SelectedColumns.json'
+# Load your Europe data
+europe_df = pd.read_json('data/json-Europe-SelectedColumns.json', lines=True)
 
-    try:
-        # Load your Europe data
-        europe_df = pd.read_json(json_file_path, lines=True)
+# Replace 'date', 'total_vaccinations', and 'location' with your actual column names
+# Convert the 'date' column to datetime if it's not already
+europe_df['date'] = pd.to_datetime(europe_df['date'])
 
-        # Replace 'date', 'total_vaccinations', and 'location' with your actual column names
-        # Convert the 'date' column to datetime if it's not already
-        europe_df['date'] = pd.to_datetime(europe_df['date'])
+# Extract year and month from the date
+europe_df['year_month'] = europe_df['date'].dt.to_period('M').astype(str)  # Convert Period to string
 
-        # Extract year and month from the date
-        europe_df['year_month'] = europe_df['date'].dt.to_period('M').astype(str)  # Convert Period to string
+# Group by location and year_month, aggregating total vaccinations
+grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False).agg({
+    'iso_code': 'first',
+    'continent': 'first',
+    'date': 'first',
+    'total_cases': 'sum',
+    'population': 'first',
+    'total_vaccinations': 'sum',
+    'people_vaccinated': 'sum',
+    'people_fully_vaccinated': 'sum',
+    'total_vaccinations_per_hundred': 'sum',
+    'people_vaccinated_per_hundred': 'sum',
+    'people_fully_vaccinated_per_hundred': 'sum'
+})
 
-        # Group by location and year_month, aggregating total vaccinations
-        grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False).agg({
-            'iso_code': 'first',
-            'continent': 'first',
-            'date': 'first',
-            'total_cases': 'sum',
-            'population': 'first',
-            'total_vaccinations': 'sum',
-            'people_vaccinated': 'sum',
-            'people_fully_vaccinated': 'sum',
-            'total_vaccinations_per_hundred': 'sum',
-            'people_vaccinated_per_hundred': 'sum',
-            'people_fully_vaccinated_per_hundred': 'sum'
-        })
+# Create a choropleth using Plotly Express based on total vaccinations
+fig = px.choropleth(grouped_df,
+                    locations='location',
+                    locationmode='country names',
+                    color='total_vaccinations',
+                    hover_name='location',
+                    hover_data=grouped_df.columns,
+                    animation_frame='year_month',
+                    color_continuous_scale='Viridis',
+                    title="Total Vaccinations Choropleth Across Locations in Europe",
+                    width=1200,
+                    height=800,
+                    projection='natural earth'
+                    )
 
-        # Create a choropleth using Plotly Express based on total vaccinations
-        fig = px.choropleth(grouped_df,
-                            locations='location',
-                            locationmode='country names',
-                            color='total_vaccinations',
-                            hover_name='location',
-                            hover_data=grouped_df.columns,
-                            animation_frame='year_month',
-                            color_continuous_scale='Viridis',
-                            title="Total Vaccinations Choropleth Across Locations in Europe",
-                            width=1200,
-                            height=800,
-                            projection='natural earth'
-                            )
+# Use Europe-specific projection and set the initial center and zoom
+fig.update_geos(
+    projection_type="natural earth",
+    center=dict(lon=10, lat=50),
+    scope="europe",
+)
 
-        # Use Europe-specific projection and set the initial center and zoom
-        fig.update_geos(
-            projection_type="natural earth",
-            center=dict(lon=10, lat=50),
-            scope="europe",
-        )
 
-        return fig  # Return your Plotly figure object
-    except FileNotFoundError:
-        print(f"File '{json_file_path}' not found. Check if the file path is correct.")
-        return None
-
+# Show the plot
+return fig
