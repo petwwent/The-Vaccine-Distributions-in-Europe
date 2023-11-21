009371625fdff12fb59844441cd01317b101acea
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-21 23:06:04 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 import pandas as pd
 import json
+import os
 
 app = Flask(__name__)
+
+# Get the absolute path to the templates directory
+template_dir = os.path.abspath('templates')
+
+# Set the template folder for Flask explicitly
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def index():
@@ -13,4 +20,6 @@ def index():
     return render_template('index.html', data=json.dumps(data))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # Specify the port to run the application
+    app.run(debug=True, port=5000)
+
+        
