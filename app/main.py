121ec26abfe8c8503fd9154bb5675c11b8ee864b
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-21 03:56:17 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def index():
     with open('data/converted_data.json') as f:
         data = json.load(f)
 
-    return render_template('index.html', data=json.dumps(data))
+    return render_template('templates/index.html', data=json.dumps(data))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)  # Specify the port to run the application
