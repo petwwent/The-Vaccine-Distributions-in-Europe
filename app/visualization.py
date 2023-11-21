f76c206546da28370f4e2b72b30b6acd98b7771c
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-21 03:44:18 +0100

    Update visualization.py

--- app/visualization.py
+++ app/visualization.py
@@ -6,17 +6,16 @@ import plotly.graph_objects as go
 
 def construct_choropleth():
 # Load your Europe data
-europe_df = pd.read_json('data/json-Europe-SelectedColumns.json', lines=True)
+europe_df = pd.read_json('data/converted_data.json')
 
-# Replace 'date', 'total_vaccinations', and 'location' with your actual column names
 # Convert the 'date' column to datetime if it's not already
-europe_df['date'] = pd.to_datetime(europe_df['date'])
+df['date'] = pd.to_datetime(df['date'])
 
 # Extract year and month from the date
-europe_df['year_month'] = europe_df['date'].dt.to_period('M').astype(str)  # Convert Period to string
+df['year_month'] = df['date'].dt.to_period('M').astype(str)
 
 # Group by location and year_month, aggregating total vaccinations
-grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False).agg({
+grouped_df = df.groupby(['location', 'year_month'], as_index=False).agg({
     'iso_code': 'first',
     'continent': 'first',
     'date': 'first',
@@ -31,25 +30,26 @@ grouped_df = europe_df.groupby(['location', 'year_month'], as_index=False).agg({
 })
 
 # Create a choropleth using Plotly Express based on total vaccinations
-fig = px.choropleth(grouped_df,
-                    locations='location',
-                    locationmode='country names',
-                    color='total_vaccinations',
-                    hover_name='location',
-                    hover_data=grouped_df.columns,
-                    animation_frame='year_month',
-                    color_continuous_scale='Viridis',
-                    title="Total Vaccinations Choropleth Across Locations in Europe",
-                    width=1200,
-                    height=800,
-                    projection='natural earth'
-                    )
+fig = px.choropleth(
+    grouped_df,
+    locations='location',
+    locationmode='country names',
+    color='total_vaccinations',
+    hover_name='location',
+    hover_data=grouped_df.columns,
+    animation_frame='year_month',
+    color_continuous_scale='Viridis',
+    title="Total Vaccinations Choropleth Across Locations",
+    width=1200,
+    height=800,
+    projection='natural earth'
+)
 
 # Use Europe-specific projection and set the initial center and zoom
 fig.update_geos(
     projection_type="natural earth",
     center=dict(lon=10, lat=50),
-    scope="europe",
+    scope="europe"
 )
 
 
