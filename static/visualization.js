3ebf9ed5fae4e77ec6496360e8e96c16271ef711
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-23 00:05:54 +0100

    Delete static/visualization.js

--- static/visualization.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('/get-choropleth-data')  // Replace this with your actual route to fetch data
-        .then(response => response.json())
-        .then(data => {
-            Plotly.newPlot('visualization', data.data, data.layout);
-        })
-        .catch(error => console.error('Error:', error));
-});
