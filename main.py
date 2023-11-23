1207796e0c0e37f14691e1e2e88464028701e2c6
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-23 16:35:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 import json
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-import data as d
+from data import construct_choropleth
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    with open("data.json") as f:
-        repos = json.load(f)
-    return templates.TemplateResponse('index.html', {'request': request, 'repos': repos})
+    # Use the processed data returned by construct_choropleth() from data.py
+    processed_data = construct_choropleth()
+    return templates.TemplateResponse('index.html', {'request': request, 'data': processed_data})
 
-@app.get("/repos")
-async def repos(update: bool = False):
-    if update:
-        data = d.get_data()
-    else:
-        with open("data.json") as file:
-            data = json.load(file)
-       
     
     return data
