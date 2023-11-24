9fce9d55f1f1fe83999525b8dc400517a67135be
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-24 01:11:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
-from data import construct_choropleth
+import os
 import uvicorn
+from visualization import construct_choropleth  # Import the function
 
 app = Flask(__name__)
 
+# Get the absolute path to the templates directory
+template_dir = os.path.abspath('templates')
+
+# Set the template folder for Flask explicitly
+app = Flask(__name__, template_folder=template_dir)
+
 @app.route('/')
 def index():
-    encoded_choropleth = construct_choropleth()
-    return render_template('index.html', encoded_choropleth=encoded_choropleth)
+    return render_template('index.html')
+
+@app.route('/get-choropleth-data')
+def get_choropleth_data():
+    choropleth_data = construct_choropleth()
+    return jsonify(choropleth_data)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
