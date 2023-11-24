12757a57727318caaee7018bf96b356d8ca52456
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-24 02:19:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, FileResponse
 from visualization import construct_choropleth  # Import the function
 import os
 
@@ -7,11 +7,10 @@ app = FastAPI()
 # Get the path to the current file directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@app.get("/", response_class=Response)
+@app.get("/", response_class=FileResponse)
 async def index():
     # Assuming you have an index.html file in the 'templates' directory
-    with open(os.path.join(dir_path, "templates/index.html"), "r") as file:
-        return Response(content=file.read(), media_type="text/html")
+    return FileResponse(os.path.join(dir_path, "templates/index.html"))
 
 @app.get("/get-choropleth-data")
 async def get_choropleth_data():
