9f6e43f80c06db5067a84146da333999f95cf6a4
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-25 20:20:16 +0100

    Create about us.php

---
+++ templates/about us.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The-Vaccine-Distributions-in-Europe From 2021-2023</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="/static/style.css">
+    <!-- Include Plotly JavaScript library -->
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
+
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">The-Vaccine-Distributions-in-Europe From 2021-2023</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about us.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about App.php">About App</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <!-- Navigation menu, as before -->
+    </nav>
+    <div class="Us-container">
+        <h1>About Us</h1>
+        <p>Key User Group</p>
+        <p>The Director of European Centre for Disease Prevention and Control: The Director of ECDC is responsible for leading the ECDC’s work in strengthening Europe’s defence against infectious disease.</p>
+    </div>
+     
+</body>
