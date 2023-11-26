21a3e07d2298278645cd34fd13124f9097b3be96
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-26 01:06:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from visualization import construct_choropleth  # Import the function
 import os
+import json
+import uvicorn
 
 app = FastAPI()
 
@@ -31,7 +33,14 @@ async def serve_static(file_path: str):
     if os.path.exists(static_file_path):
         return FileResponse(static_file_path)
 
+@app.get("/get-search-data")
+async def get_search_data(location: str = Query(None), date: str = Query(None)):
+    # Read data from data.json based on provided location and date
+    with open(os.path.join(dir_path, "data.json")) as json_file:
+        data = json.load(json_file)
+        search_result = [item for item in data if item["location"] == location and item["date"] == date]
+    return search_result
+
 if __name__ == "__main__":
-    import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=5000)
