0c044b8ea3e26d93c7c661cbc078d2ccb8b1a21c
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-26 22:49:40 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse, FileResponse
+from visualization import construct_choropleth  # Import the function
+from typing import Optional
+import os
+import uvicorn
+
+app = FastAPI()
+
+# Get the path to the current file directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+@app.get("/", response_class=FileResponse)
+async def index():
+    return FileResponse(os.path.join(dir_path, "templates/index.html"))
+
+@app.get("/get-choropleth-data")
+async def get_choropleth_data(location: Optional[str] = Query(None), year_month: Optional[str] = Query(None)):
+    choropleth_data = construct_choropleth(location=location, year_month=year_month)
+    return JSONResponse(content=choropleth_data)
+
+@app.get("/aboutus", response_class=FileResponse)
+async def aboutus():
+    return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
+
+@app.get("/aboutapp", response_class=FileResponse)
+async def aboutapp():
+    return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
+
+@app.get("/static/{file_path:path}")
+async def serve_static(file_path: str):
+    static_file_path = os.path.join(dir_path, "static", file_path)
+    if os.path.exists(static_file_path):
+        return FileResponse(static_file_path)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
