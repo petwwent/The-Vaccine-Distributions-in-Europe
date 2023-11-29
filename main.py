7eb0b32f3c60577f42fc9675747c8260990bf045
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-29 15:57:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ app = FastAPI()
 # Get the path to the current file directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@app.get("/", response_class=FileResponse)
+@app.get("/", response_class=HTMLResponse)
 async def index():
     return FileResponse(os.path.join(dir_path, "templates/index.html"))
 
-@app.get("/get-stacked-bar-chart", response_class=FileResponse)
-async def get_stacked_bar_chart():
-    chart = create_stacked_bar_chart('data.json')  # Replace with your data path
-    return chart.to_html()
+@app.get("/get-stacked-bar-chart", response_class=HTMLResponse)
+async def get_stacked_bar_chart(year: int = Query(None), month: int = Query(None)):
+    # Construct the file path or use your specific data loading process
+    data_file_path = 'data.json'  # Replace with your actual file path
+    
+    chart = create_stacked_bar_chart(data_file_path, selected_year=year, selected_month=month)
+    
+    return HTMLResponse(content=chart.to_html(), status_code=200)
 
-@app.get("/aboutus", response_class=FileResponse)
+@app.get("/aboutus", response_class=HTMLResponse)
 async def aboutus():
     return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
 
-@app.get("/aboutapp", response_class=FileResponse)
+@app.get("/aboutapp", response_class=HTMLResponse)
 async def aboutapp():
     return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
 
