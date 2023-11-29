b4f0b9a03d6f1d087c8d6bb35556ff7fbc570979
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-29 16:04:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import JSONResponse, FileResponse
 from visualization import create_stacked_bar_chart  # Import the function
 import os
 import uvicorn
@@ -9,24 +9,26 @@ app = FastAPI()
 # Get the path to the current file directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=FileResponse)
 async def index():
     return FileResponse(os.path.join(dir_path, "templates/index.html"))
 
-@app.get("/get-stacked-bar-chart", response_class=HTMLResponse)
+@app.get("/get-stacked-bar-chart")
 async def get_stacked_bar_chart(year: int = Query(None), month: int = Query(None)):
     # Construct the file path or use your specific data loading process
     data_file_path = 'data.json'  # Replace with your actual file path
     
-    chart = create_stacked_bar_chart(data_file_path, selected_year=year, selected_month=month)
+    # Generate the chart data
+    chart_data = create_stacked_bar_chart(data_file_path, selected_year=year, selected_month=month)
     
-    return HTMLResponse(content=chart.to_html(), status_code=200)
+    # Return the chart data as a JSON response
+    return JSONResponse(content=chart_data, status_code=200)
 
-@app.get("/aboutus", response_class=HTMLResponse)
+@app.get("/aboutus", response_class=FileResponse)
 async def aboutus():
     return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
 
-@app.get("/aboutapp", response_class=HTMLResponse)
+@app.get("/aboutapp", response_class=FileResponse)
 async def aboutapp():
     return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
 
@@ -35,6 +37,5 @@ async def serve_static(file_path: str):
     static_file_path = os.path.join(dir_path, "static", file_path)
     if os.path.exists(static_file_path):
         return FileResponse(static_file_path)
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
