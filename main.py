3a5d8a5d25b49f7cdd6537d821fceb1b04896cbf
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-29 16:18:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from visualization import create_stacked_bar_chart  # Import the function
 import os
 import uvicorn
@@ -9,11 +9,11 @@ app = FastAPI()
 # Get the path to the current file directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@app.get("/", response_class=FileResponse)
+@app.get("/", response_class=HTMLResponse)
 async def index():
     return FileResponse(os.path.join(dir_path, "templates/index.html"))
 
-@app.get("/get-stacked-bar-chart")
+@app.get("/get-stacked-bar-chart", response_class=HTMLResponse)
 async def get_stacked_bar_chart(year: int = Query(None), month: int = Query(None)):
     # Construct the file path or use your specific data loading process
     data_file_path = 'data.json'  # Replace with your actual file path
@@ -23,19 +23,14 @@ async def get_stacked_bar_chart(year: int = Query(None), month: int = Query(None
     # Convert Plotly figure to HTML content
     chart_html = chart_data.to_html(full_html=False, include_plotlyjs='cdn')
     
-    # Save HTML content to a file
-    html_file_path = 'chart.html'
-    with open(html_file_path, 'w') as file:
-        file.write(chart_html)
-    
-    # Return the file as a FileResponse
-    return FileResponse(path=html_file_path, media_type='text/html', filename='stacked_bar_chart.html')
+    # Return the HTML content directly as an HTMLResponse
+    return HTMLResponse(content=chart_html)
 
-@app.get("/aboutus", response_class=FileResponse)
+@app.get("/aboutus", response_class=HTMLResponse)
 async def aboutus():
     return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
 
-@app.get("/aboutapp", response_class=FileResponse)
+@app.get("/aboutapp", response_class=HTMLResponse)
 async def aboutapp():
     return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
 
@@ -44,5 +39,7 @@ async def serve_static(file_path: str):
     static_file_path = os.path.join(dir_path, "static", file_path)
     if os.path.exists(static_file_path):
         return FileResponse(static_file_path)
+
 if __name__ == "__main__":
+    
     uvicorn.run(app, host="0.0.0.0", port=5000)
