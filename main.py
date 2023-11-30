bfd3939cf247446e7e439f775fd54559bb7a0bce
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-30 22:06:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.templating import Jinja2Templates
-from visualization import create_stacked_bar_chart
+from fastapi import FastAPI, Query
+from fastapi.responses import FileResponse, HTMLResponse
+from visualization import create_stacked_bar_chart  # Import the function from visualization.py
 import os
+import uvicorn
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-data_file_path = 'data.json'
+
+# Get the path to the current file directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
+data_file_path = 'data.json' 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    chart = create_stacked_bar_chart(data_file_path)
+async def index():
+    chart = create_stacked_bar_chart(data_file_path)  # Call the function to generate the chart
+    
+    # Convert the figure to HTML and return it as the response
     chart_html = chart.to_html(full_html=False, include_plotlyjs='cdn')
-    return templates.TemplateResponse("index.html", {"request": request, "chart_html": chart_html})
+    return HTMLResponse(content=chart_html)
 
-@app.get("/aboutus", response_class=HTMLResponse)
-async def aboutus(request: Request):
-    return templates.TemplateResponse("aboutus.html", {"request": request})
 
-@app.get("/aboutapp", response_class=HTMLResponse)
-async def aboutapp(request: Request):
-    return templates.TemplateResponse("aboutapp.html", {"request": request})
+@app.get("/aboutus", response_class=FileResponse)
+async def aboutus():
+    return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
 
-@app.get("/static/{file_path:path}", response_class=HTMLResponse)
-async def serve_static(file_path: str, request: Request):
+@app.get("/aboutapp", response_class=FileResponse)
+async def aboutapp():
+    return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
+
+@app.get("/static/{file_path:path}", response_class=FileResponse)
+async def serve_static(file_path: str):
     static_file_path = os.path.join(dir_path, "static", file_path)
     if os.path.exists(static_file_path):
-        return templates.TemplateResponse(static_file_path, {"request": request})
+        return FileResponse(static_file_path)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
+
