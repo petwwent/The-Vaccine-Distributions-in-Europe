5bda82750a43eb13f714dc3d3f043cec740b362d
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-30 20:20:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from visualization import create_stacked_bar_chart
 import os
