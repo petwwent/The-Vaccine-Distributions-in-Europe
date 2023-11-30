089660bb4f5b9ae878c808ecbea43f26dcce9702
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-11-30 22:12:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 from fastapi import FastAPI, Query
 from fastapi.responses import FileResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
 from visualization import create_stacked_bar_chart  # Import the function from visualization.py
 import os
 import uvicorn
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 data_file_path = 'data.json'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@app.get("/", response_class=HTMLResponse)
-async def index():
-    chart = create_stacked_bar_chart(data_file_path)
-    chart_html = chart.to_html(full_html=False, include_plotlyjs='cdn')
-
+def get_index_with_chart(chart_html: str) -> str:
     # Read the content of index.html
     with open(os.path.join(dir_path, "templates/index.html"), "r") as file:
         html_content = file.read()
 
     # Inject the chart HTML into the index.html content
     html_content_with_chart = html_content.replace("{{ chart_here }}", chart_html)
-    return HTMLResponse(content=html_content_with_chart)
+    return html_content_with_chart
 
-@app.get("/aboutus", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    chart = create_stacked_bar_chart(data_file_path)
+    chart_html = chart.to_html(full_html=False, include_plotlyjs='cdn')
+
+    return HTMLResponse(content=get_index_with_chart(chart_html), status_code=200)
+
+@app.get("/aboutus", response_class=FileResponse)
 async def aboutus():
-    return templates.TemplateResponse("aboutus.html")
+    return FileResponse(os.path.join(dir_path, "templates/aboutus.html"))
 
-@app.get("/aboutapp", response_class=HTMLResponse)
+@app.get("/aboutapp", response_class=FileResponse)
 async def aboutapp():
-    return templates.TemplateResponse("aboutapp.html")
-
+    return FileResponse(os.path.join(dir_path, "templates/aboutapp.html"))
 
 @app.get("/static/{file_path:path}", response_class=FileResponse)
 async def serve_static(file_path: str):
