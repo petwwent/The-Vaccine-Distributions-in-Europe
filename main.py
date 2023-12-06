5dca767d1865dc0fcaa1efdbeec4c8edde00cb57
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-12-06 21:14:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, Query
 from fastapi.responses import HTMLResponse, FileResponse
 from visualization import create_stacked_bar_chart
 import os
