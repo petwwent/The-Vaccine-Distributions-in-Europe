0170c704464333b32014aa79fb9ea48fd3562a9e
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-12-06 17:40:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
-from fastapi import FastAPI, File
-from fastapi.responses import HTMLResponse, FileResponse
-from visualization import create_stacked_bar_chart  # Import the function from visualization.py
+from fastapi import FastAPI, HTMLResponse, FileResponse
+from visualization import create_stacked_bar_chart
+import os
+import uvicorn
 
 app = FastAPI()
 
+# Get the path to the current file directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # Function to generate the stacked bar chart HTML
 def generate_chart_html():
-    # Replace 'data_file_path' with the actual path to your data file
+    # Replace this function with your actual logic to generate the chart HTML
     data_file_path = 'data.json'
     chart = create_stacked_bar_chart(data_file_path)
     chart_html = chart.to_html(full_html=False, include_plotlyjs='cdn')
     return chart_html
 
-# Endpoint to serve index.html with the embedded visualization
-@app.get("/", response_class=HTMLResponse)
-async def index():
+# Endpoint to serve the stacked bar chart
+@app.get("/get-stacked-bar-chart", response_class=HTMLResponse)
+async def get_stacked_bar_chart():
     chart_html = generate_chart_html()
-    # Read the content of index.html and embed the chart HTML into it
-    with open("templates/index.html", "r") as file:
-        content = file.read().replace("<!-- INSERT_CHART -->", chart_html)
-    return HTMLResponse(content=content)
+    return HTMLResponse(content=chart_html)
 
-# Endpoint for /aboutus to serve aboutus.html
-@app.get("/aboutus", response_class=HTMLResponse)
-async def aboutus():
-    with open("templates/aboutus.html", "r") as file:
+# Endpoint for the main index page
+@app.get("/", response_class=HTMLResponse)
+async def read_main():
+    with open(os.path.join(dir_path, "templates/index.html"), "r") as file:
         content = file.read()
     return HTMLResponse(content=content)
 
-# Endpoint for /aboutapp to serve aboutapp.html
+# Endpoint for aboutus
+@app.get("/aboutus", response_class=HTMLResponse)
+async def read_aboutus():
+    with open(os.path.join(dir_path, "templates/aboutus.html"), "r") as file:
+        content = file.read()
+    return HTMLResponse(content=content)
+
+# Endpoint for aboutapp
 @app.get("/aboutapp", response_class=HTMLResponse)
-async def aboutapp():
-    with open("templates/aboutapp.html", "r") as file:
+async def read_aboutapp():
+    with open(os.path.join(dir_path, "templates/aboutapp.html"), "r") as file:
         content = file.read()
     return HTMLResponse(content=content)
 
 # Endpoint to serve static files like CSS
 @app.get("/static/{file_path:path}")
 async def serve_static(file_path: str):
-    return FileResponse(f"static/{file_path}")
+    static_file_path = os.path.join(dir_path, "static", file_path)
+    if os.path.exists(static_file_path):
+        return FileResponse(static_file_path)
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
