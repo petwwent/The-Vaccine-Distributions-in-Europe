3b75c2638b3e736f851e3756bdb1ea266b856156
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-12-19 16:01:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory, jsonify, request
 import json
 
 app = Flask(__name__)
 
-# Sample data serving routes
-@app.route('/defaultChartData')
-def default_chart_data():
-    # Read data from data.json and process it
-    with open('visualization_data/data.json', 'r') as json_file:
+# Route to serve index.html
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+# Route to serve comparison.html
+@app.route('/comparison')
+def comparison():
+    return send_from_directory('static', 'comparison.html')
+
+# Route to serve static files like CSS and JavaScript
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+# Route to serve COVID-19 data from data.json
+@app.route('/covidData')
+def covid_data():
+    # Read data from data.json
+    with open('static/data.json', 'r') as json_file:
         data = json.load(json_file)
 
-    # Process data for default chart: Sort by dates in descending order
-    default_data = data['comparisonChartData']
-    sorted_data = sorted(default_data, key=lambda x: x['label'], reverse=True)
+    return jsonify(data)
 
-    # Assign colors based on population (example colors)
-    color_scale = {
-        "Location A": "blue",
-        "Location B": "green",
-        "Location C": "red"
-        # Add more colors for other locations as needed
-    }
+# Route to handle comparison based on locations and date
+@app.route('/compareData', methods=['POST'])
+def compare_data():
+    data = request.json  # Received JSON data from JavaScript
 
-    # Assign colors to data based on population (replace 'value' with actual population key)
-    for item in sorted_data:
-        for key in item.keys():
-            if key != 'label':
-                item[key + '_color'] = color_scale[key]
-    
-    return jsonify(sorted_data)
+    # Process data for comparison based on selected locations and date (data contains selected locations and date)
+    selected_locations = data.get('locations', [])
+    selected_date = data.get('date')
 
-# Your other routes and logic go here...
+    # Read data from data.json
+    with open('static/data.json', 'r') as json_file:
+        all_data = json.load(json_file)
+
+    # Filter data based on selected locations and date
+    compared_data = [entry for entry in all_data if entry['location'] in selected_locations and entry['date'] == selected_date]
+
+    return jsonify(compared_data)
+
+# Your other routes and logic...
 
 if __name__ == '__main__':
     app.run()
