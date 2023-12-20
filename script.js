4a7be2ff5af5df56425c46458d8455b621d4d2e8
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-12-20 14:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,149 @@
-// Fetching data from data.json in the root directory
-d3.json('data.json')
-  .then(data => {
-    console.log('Data:', data); // Log loaded data to verify
+// Load data using d3.json
+d3.json('data.json').then(data => {
+  // Convert 'date' to a JavaScript Date object and 'total_vaccinations' to numbers
+  data.forEach(d => {
+    d.date = new Date(d.date);
+    d.total_vaccinations = +d.total_vaccinations || 0;
+  });
 
-    const svg = d3.select('#chart'),
-      margin = { top: 40, right: 40, bottom: 60, left: 60 },
-      width = +svg.attr('width') - margin.left - margin.right,
-      height = +svg.attr('height') - margin.top - margin.bottom;
+  // Obtain min and max dates from the dataset
+  const minDate = d3.min(data, d => d.date);
+  const maxDate = d3.max(data, d => d.date);
 
-    const g = svg.append('g')
+  // Initialize variables for slider and play/pause functionality
+  let isPlaying = false;
+  let playInterval;
+
+  // Create a date slider
+  $("#slider").slider({
+    range: true,
+    min: minDate.getTime(),
+    max: maxDate.getTime(),
+    step: 24 * 60 * 60 * 1000,
+    values: [minDate.getTime(), maxDate.getTime()],
+    slide: function(event, ui) {
+      const startDate = new Date(ui.values[0]);
+      const endDate = new Date(ui.values[1]);
+
+      $('#startLabel').text(startDate.toDateString());
+      $('#endLabel').text(endDate.toDateString());
+
+      if (!isPlaying) {
+        updateChart(startDate, endDate, data);
+      }
+    }
+  });
+
+  // Initialize chart with default date range using full dataset
+  updateChart(minDate, maxDate, data);
+
+  // Play button click event
+  $('#playButton').on('click', function() {
+    if (!isPlaying) {
+      isPlaying = true;
+      playInterval = setInterval(() => {
+        const sliderValues = $("#slider").slider('option', 'values');
+        const currentEndDate = new Date(sliderValues[1]);
+        const nextEndDate = new Date(currentEndDate.getTime() + 24 * 60 * 60 * 1000);
+
+        if (nextEndDate <= maxDate) {
+          const startDate = new Date(sliderValues[0]);
+          $("#slider").slider('values', [startDate.getTime(), nextEndDate.getTime()]);
+          updateChart(startDate, nextEndDate, data);
+        } else {
+          stopSlider();
+        }
+      }, 1000);
+    }
+  });
+
+  // Pause button click event
+  $('#pauseButton').on('click', stopSlider);
+
+  // Function to stop the slider
+  function stopSlider() {
+    clearInterval(playInterval);
+    isPlaying = false;
+  }
+
+  // Function to update the chart based on filtered data
+  function updateChart(startDate, endDate, data) {
+    const filteredData = data.filter(d => d.date >= startDate && d.date <= endDate);
+
+    // Sort the filteredData by total_vaccinations in descending order
+    filteredData.sort((a, b) => b.total_vaccinations - a.total_vaccinations);
+
+
+    // Remove existing chart
+    d3.select('#chart').selectAll('*').remove();
+
+    // Chart dimensions and other elements setup
+    const margin = { top: 50, right: 50, bottom: 100, left: 150 };
+    const width = 1000 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
+
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const locations = data.map(d => d.location);
-    const totalVaccinations = data.map(d => +d.total_vaccinations || 0);
-
+    // Create scales
     const x = d3.scaleBand()
-      .domain(locations)
+      .domain(filteredData.map(d => d.location))
       .range([0, width])
-      .padding(0.1);
+      .padding(0.2);
 
     const y = d3.scaleLinear()
-      .domain([0, d3.max(totalVaccinations)])
+      .domain([0, d3.max(filteredData, d => d.total_vaccinations)])
+      .nice()
       .range([height, 0]);
 
-    g.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x))
+    // Create axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y).ticks(10);
+
+    // Append axes to SVG
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis)
       .selectAll('text')
       .attr('transform', 'rotate(-45)')
       .style('text-anchor', 'end');
 
-    g.append('g')
-      .call(d3.axisLeft(y).ticks(10))
-      .append('text')
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
+
+    // Add axis labels
+    svg.append('text')
+      .attr('class', 'axis-label')
+      .attr('text-anchor', 'middle')
+      .attr('x', width / 2)
+      .attr('y', height + margin.bottom / 2)
+      .text('Location');
+
+    svg.append('text')
+      .attr('class', 'axis-label')
+      .attr('text-anchor', 'middle')
       .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '-3em')
-      .attr('text-anchor', 'end')
+      .attr('x', -height / 2)
+      .attr('y', -margin.left + 15)
       .text('Total Vaccinations');
 
-    g.selectAll('.bar')
-      .data(data)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => x(d.location))
-      .attr('width', x.bandwidth())
-      .attr('y', d => y(+d.total_vaccinations || 0))
-      .attr('height', d => height - y(+d.total_vaccinations || 0))
-      .attr('fill', 'steelblue');
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+    // Create bars based on the sorted data
+  svg.selectAll('.bar')
+  .data(filteredData)
+  .enter().append('rect')
+  .attr('class', 'bar')
+  .attr('x', d => x(d.location))
+  .attr('width', x.bandwidth())
+  .attr('y', d => y(d.total_vaccinations))
+  .attr('height', d => height - y(d.total_vaccinations))
+  .attr('fill', 'steelblue')
+  .append('title')
+  .text(d => `${d.location}: ${d.total_vaccinations} vaccinations`);
+ }
+});
