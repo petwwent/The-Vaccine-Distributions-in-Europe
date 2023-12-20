1a9431dac4983716b86f9425607678e7599b0758
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2023-12-20 02:41:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+# Route for serving the index.html file
 @app.route('/')
 def index():
     return send_file('index.html')
 
+# Route for serving the data.json file
+@app.route('/data.json')
+def get_data():
+    return send_file('data.json')
+
+# Route for serving the script.js file
 @app.route('/script.js')
-def script():
+def get_script():
     return send_file('script.js')
 
 if __name__ == '__main__':
