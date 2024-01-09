c15ae2cdac54be23e2f00ca77b5605cbb6f18cdc
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2024-01-09 22:51:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-# Route for serving the index.html file
-@app.route('/static/')
+# Route for serving index.html
+@app.route('/')
 def index():
-    return send_file('index.html')
+    return send_file('static/index.html')
 
-# Route for serving the data.json file
-@app.route('/static/data.json')
-def get_data():
-    return send_file('data.json')
-
-# Route for serving the script.js file
-@app.route('/static/script.js')
+# Route for serving script.js
+@app.route('/script.js')
 def get_script():
-    return send_file('script.js')
+    return send_file('static/script.js')
 
-# Route for serving the styles.css file
-@app.route('/static/styles.css')
+# Route for serving styles.css
+@app.route('/styles.css')
 def get_styles():
-    return send_file('styles.css')
+    return send_file('static/styles.css')
 
+# Route for serving data.json
+@app.route('/data.json')
+def get_data():
+    return send_file('static/data.json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
